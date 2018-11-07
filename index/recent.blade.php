589995bd6dc5eb1c875bf38e36155bb444efa4a7
marco xot <marco.sottana@gmail.com>
2018-11-07 09:56:07 +0100

    git lfs

---
+++ index/recent.blade.php
@@ -0,0 +1,52 @@
+<section class="land-see-recent-content relative mx-auto py4">
+			<h4 class="land-see-section-header ml1">Recent Stories</h4>
+			<div class="clearfix pt2">
+				<amp-position-observer on="enter:recentSlideIn.start" layout="nodisplay">
+				</amp-position-observer>
+				<ul class="list-reset m0 p0 center justify-center fit">
+					<li class="px1 mt1 block align-top land-see-pre-animate land-see-post-item land-see-post-item-wide land-see-post-featured col land-see-recent">
+						<a href="kim-myeong.amp.html" class="text-decoration-none">
+							<amp-img class="land-see-post-image cover" alt="Studio Serif, interior." media="" width="409" height="305" layout="responsive" src="../img/land-see/content/studio-310px.jpg">
+							</amp-img>
+						</a>
+						<a class="land-see-post-category land-see-page-navigation inline-block pt1 underline caps" href="stories.amp.html?category&#x3D;design#land-see-design">Design</a>
+						<h4 class="land-see-post-title ampstart-title-sm pt1"><a class="text-decoration-none" href="kim-myeong.amp.html">Office Tour: Studio Serif</a></h4>
+						<p class="ampstart-caption pt1 pb3">In the heart of the southeast, Studio Serif stands out among its competitors for its complex mixture of hand-drawn and computer-generated art coming together in beautifully, cohesive designs.</p>
+					</li>
+					<li class="px1 mt1 block align-top land-see-pre-animate land-see-post-item land-see-post-item-wide land-see-post-featured col land-see-recent">
+						<a href="kim-myeong.amp.html" class="text-decoration-none">
+							<amp-img class="land-see-post-image cover" alt="Build a killer outfit." media="" width="409" height="370" layout="responsive" src="../img/land-see/content/retail-shop-370px.jpg">
+							</amp-img>
+						</a>
+						<a class="land-see-post-category land-see-page-navigation inline-block pt1 underline caps" href="stories.amp.html?category&#x3D;fashion#land-see-fashion">Fashion</a>
+						<h4 class="land-see-post-title ampstart-title-sm pt1"><a class="text-decoration-none" href="kim-myeong.amp.html">New Textile &amp; Basics Shop Opens in Soho</a></h4>
+						<p class="ampstart-caption pt1 pb3">Everyone’s wardrobe needs the well-fitting, structured, and neutral basics to provide the start for the building of a killer outfit.</p>
+					</li>
+					<li class="px1 mt1 block align-top land-see-pre-animate land-see-post-item land-see-post-item-wide col col-6 land-see-recent">
+						<a href="kim-myeong.amp.html" class="text-decoration-none">
+							<amp-img class="land-see-post-image cover" alt="Feel the sand on your feet, not your wardrobe weight." media="" width="409" height="370" layout="responsive" src="../img/land-see/content/travel-accessories-outfits-2-400px.jpg">
+							</amp-img>
+						</a>
+						<a class="land-see-post-category land-see-page-navigation inline-block pt1 underline caps" href="stories.amp.html?category&#x3D;fashion#land-see-fashion">Fashion</a>
+						<h4 class="land-see-post-title ampstart-title-sm pt1"><a class="text-decoration-none" href="kim-myeong.amp.html">How to Pack Light When Traveling</a></h4>
+						<p class="ampstart-caption pt1 pb3">Whether it’s a short weekend getaway or an extended holiday, choosing carefully from your accessories can help lighten your load while expanding your repertory.</p>
+					</li>
+					<li class="px1 mt1 block align-top land-see-pre-animate land-see-post-item land-see-post-item-wide col col-6 land-see-recent">
+						<a href="kim-myeong.amp.html" class="text-decoration-none">
+							<amp-img class="land-see-post-image cover" alt="Epression of aesthetics." media="" width="409" height="305" layout="responsive" src="../img/land-see/content/timeless-design-350px.jpg">
+							</amp-img>
+						</a>
+						<a class="land-see-post-category land-see-page-navigation inline-block pt1 underline caps" href="stories.amp.html?category&#x3D;design#land-see-design">Design</a>
+						<h4 class="land-see-post-title ampstart-title-sm pt1"><a class="text-decoration-none" href="kim-myeong.amp.html">Timeless Design: A History of Watches</a></h4>
+						<p class="ampstart-caption pt1 pb3">A fashionable timepiece is as much a bold statement of one’s individuality, an expression of aesthetics, as it is an implement of progress.</p>
+					</li>
+				</ul>
+				<div class="absolute z4 xs-hide sm-hide land-see-recent-accent-image">
+					<amp-position-observer on="scroll:recentAccentAnimation.seekTo(percent=event.percent)" layout="nodisplay">
+					</amp-position-observer>
+					<svg xmlns="http://www.w3.org/2000/svg" width="44" height="35" viewbox="0 0 44 35" overflow="visible">
+						<path fill="#3D6458" d="M43.535 34.74H0L21.767 0z"></path>
+					</svg>
+				</div>
+			</div>
+		</section>
