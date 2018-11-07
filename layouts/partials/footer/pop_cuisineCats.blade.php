ee5d7d4d64b462c78b261022e422a16ff853ca1f
marco xot <marco.sottana@gmail.com>
2018-11-07 09:55:06 +0100

    ?

--- layouts/partials/footer/pop_cuisineCats.blade.php
+++
@@ -1,9 +0,0 @@
-<div class="footer_cucine">
-	<h5>Random Cuisine</h5>
-	<ul>
-		@foreach($cuisineCat->archive()->inRandomOrder()->limit(6)->get() as $item0)
-		<li><a href="{{ $item0->url}}" title="{{ $item0->title}}">{{ $item0->title}}</a> </li>
-		@endforeach
-		<li><a href="{{url($lang.'/cuisineCat')}}">Other cuisines..</a></li>
-	</ul>
-</div>
